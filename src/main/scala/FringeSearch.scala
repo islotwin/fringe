eaf64ee3feef506677f044706e71b2373fd5fb7d
islotwin <iga.slotwinska@gmail.com>
2018-05-22 23:20:54 +0200

    Functional fringe search

--- src/main/scala/FringeSearch.scala
+++ src/main/scala/FringeSearch.scala
@@ -1,53 +1,67 @@
 case class FringeSearch[T](graph: Graph[T], start: Int, goal: Int, h: (Int, Graph[T]) => Double) {
 
-  var fringe = List(start)
-  var cache = Map[ Int, (Double, Option[Int])]()  // (id, (g, parent))
-  var fLimit = h(start, graph)
-
-  if(start != goal) {
-    cache = cache + (start -> (0, None), goal -> null)
-  }
-
   def run(): List[Int] = {
-    while(!(isSolved() || fringe.isEmpty)) {
-      var fMin = Double.PositiveInfinity
-      fringe.foreach(n => {
-        val visited: (Double, Option[Int]) = cache(n)
-        val f: Double = visited._1 + h(n, graph)
-        if (f > fLimit) {
-          fMin = if (f > fMin) fMin else f
-          // continue
-        }
-        else if(n != goal) {
-          val successors = graph.findChildren(n) // iterate from right
-          successors._1.foreach(s => {
-            val g = visited._1 + successors._2.find(e => e.toId == s.id).get.cost
-            if (!(cache.get(s.id).nonEmpty && cache(s.id) != null && g > cache(s.id)._1)) {
-              if (fringe.exists(id => id == s.id)) {
-                fringe = fringe.diff(List(s.id))
-              }
-              // add elem after n
-              fringe = (fringe.takeWhile(_ != n) :+ s.id) ::: fringe.dropWhile(_ != n)
-              cache = cache + (s.id -> (g, Some(n)))
-            }
-          })
-          fringe = fringe.diff(List(n))
-        }
-      })
-      if(fMin != Double.PositiveInfinity) {
-        fLimit = fMin
+    if (start != goal) {
+      val result = findPath((Map[ Int, (Double, Option[Int])]((start, (0, None)), goal -> null), List(start), Double.PositiveInfinity), h(start, graph))
+      val cache = result._1._1
+      if (isSolved(cache)) {
+        println(cache)
+        cache.toList.map(c => c._1)
       }
-    }
-    if(isSolved()) {
-      println(cache)
-      cache.toList.map(c => c._1)
+      else {
+        List()
+      }
     }
     else {
       List()
     }
   }
 
-  def isSolved() : Boolean = {
+  // (cache, fringe, fmin), fLimit
+  def findPath(tuple: (Map[ Int, (Double, Option[Int])], List[Int], Double), fLimit: Double)
+  : (( Map[ Int, (Double, Option[Int])], List[Int], Double ), Double) = {
+    val result = tuple._2
+      .foldLeft(tuple._1, tuple._2, Double.PositiveInfinity)((r: (Map[Int, (Double, Option[Int])], List[Int], Double), n: Int) => {
+        val currentCache = r._1
+        val currentFringe = r._2
+        val currentFMin = r._3
+        val visited: (Double, Option[Int]) = currentCache(n)
+        val f: Double = visited._1 + h(n, graph)
+
+        if (f > fLimit) {
+          (currentCache, currentFringe, if (f > currentFMin) currentFMin else f)
+        }
+        else if (n != goal) {
+          val successors = graph.findChildren(n) // iterate from right
+          val currentResult = successors._1.foldLeft(r._1, r._2)((rr: (Map[Int, (Double, Option[Int])], List[Int]), s: Node[T]) => {
+            val succCache = rr._1
+            val succFringe = rr._2
+            val g = visited._1 + successors._2.find(e => e.toId == s.id).get.cost
+            if (!(currentCache.get(s.id).nonEmpty && currentCache(s.id) != null && g > currentCache(s.id)._1)) {
+              (succCache + (s.id -> (g, Some(n))), (succFringe.diff(List(s.id)).takeWhile(_ != n) :+ s.id) ::: succFringe.diff(List(s.id)).dropWhile(_ != n))
+            }
+            else {
+              rr
+            }
+          })
+          (currentResult._1, currentResult._2.diff(List(n)), if (f > currentFMin) currentFMin else f)
+        }
+        else {
+          r
+        }
+      })
+
+    val fMin = result._3
+
+    if(!(isSolved(result._1) || result._2.isEmpty)) {
+      findPath(result, if(fMin != Double.PositiveInfinity) fMin else fLimit)
+    }
+    else {
+      (result, fLimit)
+    }
+  }
+
+  def isSolved(cache : Map[ Int, (Double, Option[Int])]) : Boolean = {
     cache(goal) != null
   }
 
--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,43 +1,44 @@
-
 object HelloWorld extends App {
 
   try{
-  val graph = Graph[Int]()
-    .addNode(Node[Int](1, 69))
-    .addNode(Node[Int](2, 69))
-    .addNode(Node[Int](3, 69))
-    .addNode(Node[Int](4, 69))
-    .addNode(Node[Int](5, 100))
-    .addEdge(Edge(1, 2, 1.0))
-//    .addEdge(Edge(1, 2, 1.0))
-//    .addEdge(Edge(1, 10, 5.0))
-    .addEdge(Edge(5, 3, 5.0))
-    .addEdge(Edge(1, 3, 1.0))
-    .addEdge(Edge(3, 2, 1.0))
-    .addEdge(Edge(3, 4, 1.0))
-    .addEdge(Edge(2, 4, 4.0))
-    .removeNode(1)
-    .removeNode(1)
-    
-    println(graph);
+    val graph = Graph[Int]()
+      .addNode(Node[Int](1, 69))
+      .addNode(Node[Int](2, 69))
+      .addNode(Node[Int](3, 69))
+      .addNode(Node[Int](4, 69))
+      .addNode(Node[Int](5, 100))
+      .addEdge(Edge(1, 2, 1.0))
+      .addEdge(Edge(5, 3, 5.0))
+      .addEdge(Edge(1, 3, 1.0))
+      .addEdge(Edge(3, 2, 1.0))
+      .addEdge(Edge(3, 4, 1.0))
+      .addEdge(Edge(2, 4, 4.0))
+
+    println(graph)
+
+    def h[T](cost: Int, graph: Graph[T]): Double = { 0 }
+    val fringe = FringeSearch(graph, 1, 4, h[Int])
+    val res = fringe.run()
+    println(res)
+
   }
   catch {
     case e : Exception => println(e);
   }
-  
-  
-//  println(graph)
-//
-//  val childs1 = graph.findChildren(1)
-//  println(childs1)
-//
-//  val parent2 = graph.findParents(2)
-//  println(parent2)
-//
-//  def h[T](cost: Int, graph: Graph[T]): Double = {
-//    0
-//  }
-//  val fringe = FringeSearch(graph, 1, 4, h[Int])
-//  val res = fringe.run()
-//  println(res)
+
+
+  //  println(graph)
+  //
+  //  val childs1 = graph.findChildren(1)
+  //  println(childs1)
+  //
+  //  val parent2 = graph.findParents(2)
+  //  println(parent2)
+  //
+  //  def h[T](cost: Int, graph: Graph[T]): Double = {
+  //    0
+  //  }
+  //  val fringe = FringeSearch(graph, 1, 4, h[Int])
+  //  val res = fringe.run()
+  //  println(res)
 }
